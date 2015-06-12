75ebfbc78a3407feb706ff7b617fc46438bd179d
vitor <vitor.tumolo@gmail.com>
2015-06-12 10:46:23 -0300

    #770 [SendGrid] Disparo de mail marketing via API

--- parte1.php
+++ parte1.php
@@ -2,12 +2,17 @@
 
 require_once "sendgrid/newsletter.php";
 
+//Log in do Send Grid
 $sg_user = '';
 $sg_api_key = '';
 $sendgrid = new sendgridNewsletter($sg_user, $sg_api_key, true, false);
 
+//Nome da lista de Destinatários
 $list = 'Teste3';
 $sendgrid->newsletter_lists_add($list, '');
 
+//Array para dados de um DESTINATÁRIO
 $data = array('email' => 'vitor.hugo@vlance.com.br', 'name' => 'Vitor');
+
+//Função para ADICIONAR um novo DESTINATÁRIO
 $sendgrid->newsletter_lists_email_add($list, $data);
--- parte2.php
+++ parte2.php
@@ -2,87 +2,37 @@
 
 require_once "sendgrid/newsletter.php";
 
+//Log in do Send Grid
 $sg_user = '';
 $sg_api_key = '';
-
-//Creates a new SendGrid Newsletter API object to make calls with
-/*
- * YOU CAN ALSO SET $debug to true for DEBUGGING and $curl_ssl_verify to false for disabling cert verification
- * ($user, $key , $debug = false , $curl_ssl_verify = true)
- *
- */
 $sendgrid = new sendgridNewsletter($sg_user, $sg_api_key, false, false);
 
+//Corpo e html do email
 $text = 'xxxxxxxxxxxxx';
 $html = '<html><body><img src="http://vlance.com.br/leiloeiros/img/logo.png"></body></hmtl>';
+//Assunto do Email
 $subject = 'Sucesso';
 
-/**
- * Create a new Newsletter...
- * @param string $identity - The Identity that will be used for the Newsletter being created.
- * @param string $name - The name that will be used for the Newsletter being created.
- * @param string $subject - The subject that will be used for the Newsletter being created.
- * @param string $text - The text portion of the Newsletter being created.
- * @param string $html - The html portion of the Newsletter being created.
- */
+//Entidade que controla o email
 $identity = 'vLance';
+
+//Nome do Email Marketing
 $name = 'Teste9';
 
+//Função para ADICIONAR um novo EMAIL MARKETING
 $sendgrid->newsletter_add($identity, $name, $subject, $text, $html);
 
-/**
- * Create a new Recipient List...
- * @param string $list	- Create a Recipient List with this name.
- * @param string $name	- Specify the column name for the ‘name’ associated with email addresses..
- */
-//$list = 'Teste1';
-
-//$sendgrid->newsletter_lists_add($list, '');
-
-/**
- * Add an email to an existing Recipient List.
- * @param string $list	- The list which you are adding email addresses too.
- * @param array $data	- Specify the name, email address, and additional fields to add to the specified Recipient List..
- * 	EX: $data = array(
- * 				'email'	=>	'test1@test.com',
- * 				'name'	=>	'John Doe',
- * 				'Address' => '1234 Cool St',
- * 				'Zip Code' => '90210',
- * 			);
- * must use email and name fields (other fileds are optional)
- */
-//$data = array('email' => 'vitor.hugo@vlance.com.br', 'name' => 'Vitor');
-
-//$sendgrid->newsletter_lists_email_add($list, $data);
-
-/**
- * Get an Existing Recipient List...
- * @param string $list	- Check for this particular list. (To list all Recipient Lists on your account exclude this parameter);
- */
-
+//Função para PEGAR todas as LISTAS DE EMAILS
 $lista_sendgrid = $sendgrid->newsletter_lists_get('');
 print "<pre>";
 print_r($lista_sendgrid);
-//exit;
-//$sendgrid->newsletter_lists_get($list)
 
-
-
-/**
- * Add one or more Recipient List to a Newsletter.
- * @param string $name	- This is the Newsletter to which you are adding Recipients Lists.
- * @param string $name	- This is the Recipient List that will be added to the Newsletter
- */
+//Função para ASSOCIAR uma LISTA DE EMAILS ao NEWSLETTER
 $sendgrid->newsletter_recipients_add($name, $lista_sendgrid[55]['list']);
 
-/**
- * Create a new schedule event.
- * @param string $name	- Newsletter to schedule delivery for. (If Newsletter should be sent now, include no additional parameters.);
- * @param string $at	- Date / Time to schedule newsletter Delivery.
- * 	Date / Time must be provided in ISO 8601 format (YYYY-MM-DD HH:MM:SS +-HH:MM);
- * @param string $after	- Number of minutes until delivery should occur. Must be a positive integer.
- */
-$sendgrid->newsletter_schedule_add($name, '', '');
+//Horário em formato (YYYY-MM-DDTHH:MM:SS+-HH:MM)
+//Função para DISPARAR EMAILS PROGRAMÁVEIS
+$sendgrid->newsletter_schedule_add($name, $at = '', '');
 
-//echo "<meta HTTP-EQUIV='refresh' CONTENT='3;URL=sample-newsletter.php'>";
+
 ?>
